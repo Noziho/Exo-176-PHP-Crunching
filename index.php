43873b87b9ef068eee86c344cacb6206b222fc53
Noah2211 <noah.decroix3@gmail.com>
2022-01-31 14:26:01 +0100

    printing top 10 done

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
-echo "Il ya au total: ".count($dico). " mots dans ce dictionnaire.<br><br>";
+echo "Il ya au total: " . count($dico) . " mots dans ce dictionnaire.<br><br>";
 $counter = 0;
 $counter2 = 0;
 $counter3 = 0;
-foreach ($dico as $word){
-    if (strlen($word) === 15){
+foreach ($dico as $word) {
+    if (strlen($word) === 15) {
         $counter++;
     }
 
@@ -19,14 +19,19 @@ foreach ($dico as $word){
         $counter3++;
     }
 }
-echo "Il y a: ".$counter. " mots ayant une longueur de 15 <br>";
-echo "Il y a: ".$counter2. " contenant la lettre w<br>";
-echo "Il y a: ".$counter3. " mots finissant par la lettre q<br>";
+echo "Il y a: " . $counter . " mots ayant une longueur de 15 <br>";
+echo "Il y a: " . $counter2 . " contenant la lettre w<br>";
+echo "Il y a: " . $counter3 . " mots finissant par la lettre q<br>";
 
 $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 $brut = json_decode($string, true);
 $top = $brut["feed"]["entry"]; # liste de films
-echo "<pre>";
-var_dump($top);
-echo "</pre>";
+
+foreach ($top as $key => $value) {?>
+<div>
+    <h1><?= (int)$key +1 ?></h1>
+    <p><?= $value['im:name']['label'] ?></p>
+</div>
+<?php
+}
 
